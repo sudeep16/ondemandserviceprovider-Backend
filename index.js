b2aac514dd2ba350a0e1d5ef72a99a4de5774ae7
Gunjan Shakya <gunjan.shakya3@gmail.com>
2020-06-28 12:19:17 +0545

    Database configuration

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+
+//database config
+mongoose.connect(process.env.URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}).then((db) => {
+    console.log("Successfully connected to MongoDB server");
+}, (err) => console.log(err));
+
+//error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.statusCode = 500;
+    res.json({ status: err.message });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`App is running at localhost:${process.env.PORT}`);
+});
+
+
+
