7c44060e99c139c93443976a7c6b010846d74d56
Gunjan Shakya <gunjan.shakya3@gmail.com>
2020-06-29 12:25:40 +0545

    Login API ready to go

--- routes/users.js
+++ routes/users.js
@@ -34,4 +34,33 @@ router.post("/register", (req, res, next) => {
     });
 });
 
+//login
+router.post("/login", (req, res, next) => {
+    User.findOne({
+        username: req.body.username
+    }).then((user) => {
+        if (user == null) {
+            let err = new Error("User not found");
+            err.status = 401;
+            return next(err);
+        } else {
+            bcrypt.compare(req.body.password, user.password).then((isMatch) => {
+                if (!isMatch) {
+                    let err = new Error("Password doesn't match. Try again!");
+                    err.status = 401;
+                    return next(err);
+                }
+                let token = jwt.sign({
+                    _id : user._id
+                }, process.env.SECRET);
+                res.json({
+                    status: "Login successful",
+                    token: token
+                });
+                console.log(token);
+            }).catch(next);
+        }
+    }).catch(next);
+});
+
 module.exports = router;
