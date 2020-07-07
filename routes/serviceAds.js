54bc99e983975dd625aa0461c6401f13997f85c7
Gunjan Shakya <gunjan.shakya3@gmail.com>
2020-07-07 12:41:09 +0545

    get all and get by category

--- routes/serviceAds.js
+++ routes/serviceAds.js
@@ -1,16 +1,37 @@
 const express = require("express");
 const ServiceAds = require("../models/serviceAds");
+const serviceAds = require("../models/serviceAds");
 const router = express.Router();
 
 router.route("/")
     .post((req, res, next) => {
         let serviceAds = new ServiceAds(req.body);
-        serviceAds.serviceProvider = req.users._id;
+        // serviceAds.serviceProvider = req.users._id;
         serviceAds.save()
             .then((serviceAds) => {
                 res.statusCode = 201;
                 res.json(serviceAds);
             }).catch(next);
+    })
+    .get((req,res,next) => {
+        ServiceAds.find()
+        .then((serviceAds) => {
+            res.json(serviceAds);
+        })
+        .catch((err)=>{
+            next(err);
+        });
+    });
+
+router.route("/:category")
+    .get((req, res, next) => {
+        ServiceAds.find({ category: req.params.category })
+            .then((serviceAds) => {
+                res.json(serviceAds);
+            })
+            .catch((err) => {
+                next(err);
+            });
     });
 
 module.exports = router;
