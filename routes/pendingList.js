6e79a4820977e3532ac6fc5adedac5a9b64c43b9
Gunjan Shakya <gunjan.shakya3@gmail.com>
2020-08-04 11:17:22 +0545

    Pending

--- routes/pendingList.js
+++ routes/pendingList.js
@@ -2,11 +2,11 @@ const express = require("express");
 const Pendings = require("../models/pendingList");
 const router = express.Router();
 
-router.route("/:id")
+router.route("/:username")
     .post((req, res, next) => {
         let pendings = new Pendings(req.body);
-        pendings.customerID = req.user._id;
-        pendings.serviceID = req.params.id;
+        pendings.customerID = req.params.username;
+        pendings.serviceID = req.user.id;
         pendings.save()
             .then((pendings) => {
                 res.statusCode = 201;
